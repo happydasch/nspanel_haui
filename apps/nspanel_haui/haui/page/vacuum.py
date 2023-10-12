e981ec516327863dedd50ee3d4dd849157c2f0fb
happydasch <daniel@vcard24.de>
2023-10-12 16:25:25 +0200

    updated vacuum, simplified interface

--- apps/nspanel_haui/haui/page/vacuum.py
+++ apps/nspanel_haui/haui/page/vacuum.py
@@ -16,7 +16,7 @@ class VacuumPage(HAUIPage):
     ICO_START = get_icon('mdi:play')
     ICO_PAUSE = get_icon('mdi:pause')
     ICO_STOP = get_icon('mdi:stop')
-    ICO_LOCATE = get_icon('mdi:map-marker-question')
+    ICO_LOCATE = get_icon('mdi:map-marker')
     ICO_HOME = get_icon('mdi:home')
     ICO_BATTERY = get_icon('mdi:battery')
     ICO_FAN = get_icon('mdi:fan')
@@ -28,13 +28,12 @@ class VacuumPage(HAUIPage):
     # basic info
     TXT_STATUS, TXT_BATTERY, TXT_BATTERY_ICON = (7, 'tStatus'), (8, 'tBattery'), (9, 'tBatteryIcon')
     # control
-    BTN_FAN, BTN_PAUSE, BTN_START = (10, 'bFan'), (11, 'bPause'), (12, 'bStart')
-    BTN_LOCATE, BTN_HOME = (13, 'bLocate'), (14, 'bHome')
+    BTN_FAN, BTN_ACTION, BTN_HOME = (10, 'bFan'), (11, 'bAction'), (12, 'bHome')
     # entities
-    BTN_ENTITY_1, BTN_ENTITY_2, BTN_ENTITY_3 = (15, 'bEntity1'), (16, 'bEntity2'), (17, 'bEntity3')
-    BTN_ENTITY_4, BTN_ENTITY_5 = (18, 'bEntity4'), (19, 'bEntity5')
+    BTN_ENTITY_1, BTN_ENTITY_2, BTN_ENTITY_3 = (13, 'bEntity1'), (14, 'bEntity2'), (15, 'bEntity3')
+    BTN_ENTITY_4, BTN_ENTITY_5, BTN_ENTITY_6 = (16, 'bEntity4'), (17, 'bEntity5'), (18, 'bEntity6')
 
-    NUM_ENTITIES = 5
+    NUM_ENTITIES = 6
 
     _vacuum_entity = None
     _entities = []
@@ -44,18 +43,18 @@ class VacuumPage(HAUIPage):
 
     def start_panel(self, panel):
         # set function buttons
-        vacuum_state_btn = {
+        vacuum_locate_btn = {
             'fnc_component': self.BTN_FNC_RIGHT_SEC,
-            'fnc_name': 'vacuum_state',
+            'fnc_name': 'locate',
             'fnc_args': {
-                'icon': self.ICO_START,
+                'icon': self.ICO_LOCATE,
                 'color': COLORS['component_accent'],
                 'visible': False
             }
         }
         self.set_function_buttons(
             self.BTN_FNC_LEFT_PRI, self.BTN_FNC_LEFT_SEC,
-            self.BTN_FNC_RIGHT_PRI, vacuum_state_btn)
+            self.BTN_FNC_RIGHT_PRI, vacuum_locate_btn)
         # set title and entity
         entity = None
         entity_id = panel.get('entity_id')
@@ -83,51 +82,33 @@ class VacuumPage(HAUIPage):
         features = entity.get_entity_attr('supported_features', 0)
         # add listener
         self.add_entity_listener(entity.get_entity_id(), self.callback_vacuum_entity)
+        # header locate
+        locate = False
+        if features & VacuumFeatures.LOCATE:
+            locate = True
+        self.update_function_component(self.FNC_BTN_R_SEC, visible=locate)
         # fan button
         fan = False
         if features & VacuumFeatures.FAN_SPEED:
             fan = True
         self.set_function_component(
             self.BTN_FAN, self.BTN_FAN[1], 'fan',
-            icon=self.ICO_FAN,
             color=COLORS['component_active'],
-            visible=fan)
-        # pause button
-        pause = False
-        if features & VacuumFeatures.PAUSE:
-            pause = True
-        self.set_function_component(
-            self.BTN_PAUSE, self.BTN_PAUSE[1], 'pause',
-            icon=self.ICO_PAUSE,
-            color=COLORS['component_active'],
-            visible=pause)
+            icon=self.ICO_FAN, visible=fan)
         # start/stop button
-        start = False
+        action = False
         if features & VacuumFeatures.START:
-            start = True
+            action = True
         self.set_function_component(
-            self.BTN_START, self.BTN_START[1], 'start',
-            icon=self.ICO_START,
-            color=COLORS['component_active'],
-            visible=start)
-        # locate button
-        locate = False
-        if features & VacuumFeatures.LOCATE:
-            locate = True
-        self.set_function_component(
-            self.BTN_LOCATE, self.BTN_LOCATE[1], 'locate',
-            icon=self.ICO_LOCATE,
-            color=COLORS['component_active'],
-            visible=locate)
+            self.BTN_ACTION, self.BTN_ACTION[1], 'action',
+            icon=self.ICO_START, visible=action)
         # return home button
         return_home = False
         if features & VacuumFeatures.RETURN_HOME:
             return_home = True
         self.set_function_component(
             self.BTN_HOME, self.BTN_HOME[1], 'return_home',
-            icon=self.ICO_HOME,
-            color=COLORS['component_active'],
-            visible=return_home)
+            icon=self.ICO_HOME, visible=return_home)
         # entity buttons
         total_entities = len(self._entities)
         for i in range(self.NUM_ENTITIES):
@@ -139,40 +120,62 @@ class VacuumPage(HAUIPage):
                 icon = entity.get_icon()
                 color = entity.get_color()
                 visible = True
+            else:
+                entity = None
             component = getattr(self, f'BTN_ENTITY_{i+1}')
             self.set_function_component(
-                component, component[1], 'entity',
+                component, component[1], 'entity', entity=entity,
                 icon=icon, color=color, visible=visible)
 
     def update_vacuum_entity(self):
         self.set_component_text(self.TXT_TITLE, self._title)
         if self._vacuum_entity is None:
-            self.update_function_component(self.FNC_BTN_R_SEC, visible=False)
             return
         entity = self._vacuum_entity
         state = entity.get_entity_state()
+        features = entity.get_entity_attr('supported_features', 0)
+        # status text
         status_text = self.translate_state(entity.get_entity_type(), state)
         battery_level = entity.get_entity_attr('battery_level', 0)
         battery_icon = entity.get_entity_attr('battery_icon', self.ICO_BATTERY)
-        # header function
-        icon = self.ICO_START if state == 'docked' else self.ICO_HOME
-        self.update_function_component(self.FNC_BTN_R_SEC, visible=True, icon=icon)
-        # status text
         self.set_component_text(self.TXT_STATUS, status_text)
         self.set_component_text(self.TXT_BATTERY, f'{battery_level}%')
         self.set_component_text(self.TXT_BATTERY_ICON, get_icon(battery_icon))
         # pause/start/stop button
-        pause = False
+        active = False
+        pause, start, stop = False, False, False
         icon = self.ICO_START
-        if state == 'cleaning':
-            pause = True
-        if state != 'docked':
+        if state in ['cleaning', 'returning']:
+            start = False
+            if features & VacuumFeatures.PAUSE:
+                pause = True
+            if features & VacuumFeatures.STOP:
+                stop = True
+        elif state in ['docked', 'idle']:
+            if features & VacuumFeatures.START:
+                start = True
+        active = True
+        if start:
+            icon = self.ICO_START
+        elif pause:
+            icon = self.ICO_PAUSE
+        elif stop:
             icon = self.ICO_STOP
-        color, color_pressed, back_color, back_color_pressed = self.get_button_colors(pause)
+        else:
+            icon = self.ICO_START
+            active = False
+        color, color_pressed, back_color, back_color_pressed = self.get_button_colors(active)
         self.update_function_component(
-            self.BTN_PAUSE[1], touch=pause, color=color, color_pressed=color_pressed,
+            self.BTN_ACTION[1], touch=active, color=color, color_pressed=color_pressed,
+            icon=icon, back_color_pressed=back_color_pressed)
+        # home button
+        active = False
+        if state != 'docked':
+            active = True
+        color, color_pressed, back_color, back_color_pressed = self.get_button_colors(active)
+        self.update_function_component(
+            self.BTN_HOME[1], touch=active, color=color, color_pressed=color_pressed,
             back_color_pressed=back_color_pressed)
-        self.update_function_component(self.BTN_START, icon=icon)
 
     # callback
 
@@ -182,23 +185,25 @@ class VacuumPage(HAUIPage):
         # header
         entity = self._vacuum_entity
         state = self._vacuum_entity.get_entity_state()
-        if fnc_id == self.FNC_BTN_R_SEC:
-            if state == 'docked':
-                entity.call_entity_service('start')
-            elif state == 'paused':
-                entity.call_entity_service('resume')
-            else:
-                entity.call_entity_service('return_to_base')
         # fan
-        elif fnc_name == 'fan':
-            # TODO select fan speed
-            # self._vacuum_entity.call_entity_service('set_fan_speed', 'low')
-            pass
-        # pause
-        elif fnc_name == 'pause':
-            entity.call_entity_service('pause')
+        if fnc_name == 'fan':
+            navigation = self.app.controller['navigation']
+            current = entity.get_entity_attr('fan_speed', '')
+            selection = entity.get_entity_attr('fan_speed_list', [])
+            speeds = []
+            for name in selection:
+                value = self.translate_state('vacuum', name, attr='fan_speed')
+                speeds.append({'name': name, 'value': value})
+            if len(speeds) > 0:
+                navigation.open_popup(
+                    'popup_select',
+                    title=self.translate('Select Fan Speed'),
+                    selection=speeds,
+                    selected=current,
+                    selection_callback_fnc=self.callback_fan_speed,
+                    close_on_select=True)
         # start
-        elif fnc_name == 'start':
+        elif fnc_name == 'action':
             if state == 'cleaning':
                 entity.call_entity_service('stop')
             else:
@@ -211,17 +216,23 @@ class VacuumPage(HAUIPage):
             entity.call_entity_service('return_to_base')
         # entity
         elif fnc_name == 'entity':
-            fnc_id
-            # TODO self._vacuum_entity.call_entity_service('return_to_base')
-            pass
+            entity = self._fnc_items[fnc_id]['fnc_args'].get('entity')
+            if entity is not None:
+                entity.execute()
         else:
             self.log(f'{fnc_id}, {fnc_name}')
 
     def callback_vacuum_entity(self, entity, attribute, old, new, kwargs):
         if attribute == 'state':
+            self.start_rec_cmd()
             self.update_vacuum_entity()
+            self.stop_rec_cmd(send_commands=True)
         else:
             self.log(f'Unknown vacuum entity attribute: {attribute}')
+
+    def callback_fan_speed(self, selection):
+        self.log(f'Got fan speed selection: {selection}')
+        self._vacuum_entity.call_entity_service('set_fan_speed', fan_speed=selection)
 
 
 class PopupVacuumPage(VacuumPage):
